92fbedbab59dde81571b32d1575e566a4f84e73f
v1ktorbro <vitya.abrosimoff2016@ya.ru>
2022-06-26 00:45:40 +0500

    refactor(user): remove eslint rule

--- models/user.js
+++ models/user.js
@@ -43,7 +43,6 @@ const userSchema = new mongoose.Schema({
   },
 }, { versionKey: false });
 
-// eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password').then((user) => {
     if (!user) {
